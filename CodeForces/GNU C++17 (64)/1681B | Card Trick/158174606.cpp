b8d7cc90be39d27e428bb6fcd63557e3c3964f82
adigitboy <90270749+adigitboy@users.noreply.github.com>
2022-06-05 22:13:26 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1681B | Card Trick/158174606.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+ 
+using namespace __gnu_pbds;
+ 
+template<class T>
+ 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+template<class key, class value, class cmp = std::less<key>>
+using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long t;
+    cin>>t;
+    while(t--){ 
+        long long n;
+        cin>>n;
+        vector<long long> v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        long long m;
+        cin>>m;
+        long long sum = 0;
+        for(int i=0;i<m;i++){
+            long long x;
+            cin>>x;
+            sum+=x;
+        } 
+        // cout<<sum<<" ";
+        long long idx=sum%n;
+        
+        cout<<v[idx]<<"\n";
+    }
+    return 0;
+}
