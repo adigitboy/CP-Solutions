1f258a943dc2463a60b07c63881eae2d1bdc92eb
adigitboy <90270749+adigitboy@users.noreply.github.com>
2022-06-05 22:13:49 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1604D | Moderate Modular Mode/157800889.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+ 
+using namespace __gnu_pbds;
+ 
+template<class T>
+ 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+template<class key, class value, class cmp = std::less<key>>
+using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long t;
+    cin>>t;
+    while(t--){ 
+        long long a,b;
+        cin>>a>>b;
+        if(a>b){
+            cout<<a+b<<"\n";
+        }
+        else{
+            long long val = b - (b%a)/2 ;
+            cout<<val<<"\n";
+        }
+    }
+    return 0;
+}
