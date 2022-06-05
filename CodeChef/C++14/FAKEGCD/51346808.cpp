1869340e0523832706b35d09a734530703a4b436
adigitboy <90270749+adigitboy@users.noreply.github.com>
2022-06-05 20:51:22 +0530

    Committing files

---
+++ CodeChef/C++14/FAKEGCD/51346808.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long t;
+    cin>>t;
+    while(t--){	
+        long long n;
+        cin>>n;
+        for(int i=1;i<=n;i++){
+            cout<<i<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
