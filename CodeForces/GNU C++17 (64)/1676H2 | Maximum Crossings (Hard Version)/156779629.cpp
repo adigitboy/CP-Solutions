765bbbbd3248fa5b6566b50ca63c545f5aa46fe0
adigitboy <90270749+adigitboy@users.noreply.github.com>
2022-06-05 22:14:31 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1676H2 | Maximum Crossings (Hard Version)/156779629.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+ 
+using namespace __gnu_pbds;
+ 
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+ 
+template<class key, class value, class cmp = std::less<key>>
+using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long t;
+    cin>>t;
+    while(t--){ 
+        long long n;
+        cin>>n;
+        vector<long long> v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        long long total=0;
+        ordered_set<pair<int,int>> A;
+        
+        for(int i=n-1;i>=0;i--){
+            total = total+A.order_of_key({v[i],INT_MAX});  
+            A.insert({v[i],i});
+        }
+        cout<<total<<"\n";
+    }
+    return 0;
+}
