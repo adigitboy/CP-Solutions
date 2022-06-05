b835f0c6e0769f9188aa44fa000e5c87eebb495a
adigitboy <90270749+adigitboy@users.noreply.github.com>
2022-06-05 22:19:13 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/701C | They Are Everywhere/152859593.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+ 
+using namespace __gnu_pbds;
+ 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+bool isPos(long long mid,string &s,long long sz){
+    unordered_map<char,long long> mp;
+    long long val = 0;
+    for(int i=0;i<mid;i++){
+        mp[s[i]]++;
+    }
+    val = mp.size();
+    for(int i=mid;i<s.size();i++){
+        mp[s[i-mid]]--;
+        if(mp[s[i-mid]]==0){
+            mp.erase(s[i-mid]);
+        }
+        mp[s[i]]++;
+        val = max(val,(long long)mp.size());
+    }
+    return val==sz;
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long n;
+    cin>>n;
+    string s;
+    cin>>s;
+    set<char> st;
+    for(int i=0;i<s.size();i++){
+        st.insert(s[i]);
+    }
+    long long sz = st.size();
+    
+    long long low = 1,high = s.size(),ans;
+    while(low<=high){
+        long long mid = low + (high-low)/2;
+        
+        // cout<<mid<<"\n";
+        if(isPos(mid,s,sz)){
+            ans = mid;
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+    }
+    cout<<ans<<"\n";
+    return 0;
+}
