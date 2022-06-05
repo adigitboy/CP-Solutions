afe2617a74317d9d80408320305cefedc942c296
adigitboy <90270749+adigitboy@users.noreply.github.com>
2022-06-05 22:19:03 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1665A | GCD vs LCM/153022544.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+ 
+using namespace __gnu_pbds;
+ 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long t;
+    cin>>t;
+    while(t--){ 
+        long long n;
+        cin>>n;
+        long long a = 1,b=n-3,c=1,d=1;
+        cout<<a<<" "<<b<<" "<<c<<" "<<d<<"\n";
+    }
+    return 0;
+}
