aa52c24b4adb5cdd74d3a3f47536fd3ad9dc1ec1
adigitboy <90270749+adigitboy@users.noreply.github.com>
2022-06-05 22:14:20 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1679A | AvtoBus/157167629.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+ 
+using namespace __gnu_pbds;
+ 
+template<class T>
+ 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+template<class key, class value, class cmp = std::less<key>>
+using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+ 
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long t;
+    cin>>t;
+    while(t--){ 
+        long long n;
+        cin>>n;
+        if(n%2!=0){
+            cout<<-1<<"\n";
+        }
+        else if(n<4){
+            cout<<-1<<"\n";
+        }
+        else{
+            long long val = n/2;
+            long long maxi=(val)/2;
+            long long mini=(val+2)/3;
+        
+            cout<<mini<<" "<<maxi<<"\n";
+        }
+    }
+        
+    return 0;
+}
