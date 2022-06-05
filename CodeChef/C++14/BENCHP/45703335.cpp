286e559f809cead2088084816a0cd3f53d183770
adigitboy <90270749+adigitboy@users.noreply.github.com>
2022-06-05 20:51:41 +0530

    Committing files

---
+++ CodeChef/C++14/BENCHP/45703335.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){	
+        long long n,w,wr;
+        cin>>n>>w>>wr;
+        vector<long long> v(n);
+        map<long long,long long> mp;
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+            mp[v[i]]++;
+        }
+        if(wr>=w){
+            cout<<"YES\n";
+            continue;
+        }
+        bool flag = false;
+        long long remaining = w - wr;
+        for(auto x : mp){
+            if(x.second%2==0){
+                if(x.second*x.first>=remaining){
+                    flag = true;
+                    break;
+                }
+                else{
+                    remaining -=x.second*x.first;
+                }
+            }
+        }
+        if(flag){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
+    }
+    return 0;
+}
